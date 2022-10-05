5ad38dfa23c03e2f85f869e3a1e36430072f2561
erikgreasy <erik.masny@gmail.com>
2022-10-05 11:27:55 +0200

    feat: launch animation on click

--- app/src/main/java/com/example/semestralka/MainActivity.kt
+++ app/src/main/java/com/example/semestralka/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.semestralka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animation: LottieAnimationView = findViewById(R.id.animationView)
+
+        animation.setOnClickListener {
+            launchAnimation(animation)
+        }
+    }
+
+    private fun launchAnimation(animationView: LottieAnimationView) {
+        animationView.loop(false)
+        animationView.playAnimation()
     }
 }
