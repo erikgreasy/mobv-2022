dd7d2a0128371c20f76a46822b2135fe73924ecd
erikgreasy <erik.masny@gmail.com>
2022-11-26 10:59:05 +0100

    feat: build base login view

---
+++ app/src/main/java/com/example/semestralka/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.example.semestralka
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.semestralka.databinding.FragmentBarDetailBinding
+import com.example.semestralka.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
