ada926ddb5165fbff2077d84a738091167c3cbd1
erikgreasy <erik.masny@gmail.com>
2022-12-03 18:02:34 +0100

    feat: create near bars fragment

--- app/src/main/java/com/example/semestralka/NearBarsList.kt
+++ app/src/main/java/com/example/semestralka/NearBarsList.kt
@@ -5,18 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.semestralka.databinding.FragmentFriendsBinding
 import com.example.semestralka.databinding.FragmentLoginBinding
+import com.example.semestralka.databinding.FragmentNearBarsListBinding
 
-class FriendsFragment : Fragment() {
-    private var _binding: FragmentFriendsBinding? = null
+class NearBarsList : Fragment() {
+    private var _binding: FragmentNearBarsListBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentFriendsBinding.inflate(inflater, container, false)
+        _binding = FragmentNearBarsListBinding.inflate(inflater, container, false)
 
         return binding.root
     }
